6e8a72993b99e2f6b57c7df3469bcf788ba66c2b
Paulvannan Balasubramanian <paulvannan@gmail.com>
2016-06-15 10:29:18 +0200

    Solution for verifying balanced paranthesis

---
+++ core-java/src/main/java/com/interview/parser/BalancedParanthesisAndBrackets.java
@@ -0,0 +1,56 @@
+package com.interview.parser;
+
+import java.util.Stack;
+
+public class BalancedParanthesisAndBrackets {
+	
+	/**
+	 * Valid - {(a+[b-c])}
+	 * Invalid - ([)] - No interleaving, )( - Not balanced
+	 * 
+	 * If start char ( or [ or { push into stack
+	 * If end char ) or ] or } verify that top of stack is matching start char ( or [ or {
+	 *     Pop the top char
+	 * Else
+	 * 	  Not balanced return false
+	 * 
+	 * After scanning all chars if stack is not Empty return false
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public boolean isValid(String input) {
+		if(input == null) return false;
+		if(input.isEmpty()) return true;
+		Stack<Character> charStack = new Stack<Character>();
+		for(char ch : input.toCharArray()){
+			switch(ch){
+			case '(':
+			case '[':
+			case '{':
+				charStack.push(ch);
+				break;
+			case ')':
+			case ']':
+			case '}':
+				if(charStack.isEmpty()) return false;
+				char stackTop = (char) charStack.peek();
+				switch(stackTop){
+				case '(':
+					if(ch != ')') return false;
+					break;
+				case '[':
+					if(ch != ']') return false;
+					break;
+				case '{':
+					if(ch != '}') return false;
+					break;
+				default:
+					break;
+				}
+				charStack.pop();
+			}
+		}
+		return charStack.isEmpty();
+	}
+}
---
+++ core-java/src/test/java/com/interview/parser/BalancedParanthesisTest.java
@@ -0,0 +1,45 @@
+package com.interview.parser;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BalancedParanthesisTest {
+	private BalancedParanthesis validator;
+	@Before
+	public void setUp(){
+		this.validator = new BalancedParanthesis();
+	}
+	@Test
+	public void testNullInput(){
+		assertEquals(false, validator.isValid(null));
+	}
+	@Test
+	public void testEmptyInput(){
+		assertEquals(true, validator.isValid(""));
+	}
+	@Test
+	public void testValidOnePair(){
+		assertEquals(true, validator.isValid("(a+b)"));
+	}
+	@Test
+	public void testValidMultiplePairs(){
+		assertEquals(true, validator.isValid("(a+b)*(c/d)-(f%g)"));
+	}
+	@Test
+	public void testValidNestedPairs(){
+		assertEquals(true, validator.isValid("(a+(b*c))-((d/f)%g)"));
+	}
+	@Test
+	public void testInValidOnePair(){
+		assertEquals(false, validator.isValid(")a+b("));
+	}
+	@Test
+	public void testInValidMultiplePairs(){
+		assertEquals(false, validator.isValid(")((a+b)*(c/d)-(f%g)"));
+	}
+	@Test
+	public void testInValidNestedPairs(){
+		assertEquals(false, validator.isValid("(a+()b*c))-(())d/f)%g)"));
+	}
+}
