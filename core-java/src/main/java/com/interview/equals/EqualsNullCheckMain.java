19dad3b6acabad98438d8c677870c6d42f69ac90
Paulvannan Balasubramanian <paulvannan@gmail.com>
2016-07-04 10:23:51 +0200

    No need to check for null in equals method. instanceof will always return false if the object is null

---
+++ core-java/src/main/java/com/interview/equals/EqualsNullCheckMain.java
@@ -0,0 +1,10 @@
+package com.interview.equals;
+
+public class EqualsNullCheckMain {
+
+	public static void main(String[] args) {
+		Object someObject = null;
+		System.out.println( "Is String? " + (someObject instanceof String) );
+	}
+
+}
