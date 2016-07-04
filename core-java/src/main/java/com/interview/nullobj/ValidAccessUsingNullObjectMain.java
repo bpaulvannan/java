9e821bdc6f57382137c3c62131a1022950154f35
Paulvannan Balasubramanian <paulvannan@gmail.com>
2016-07-04 10:22:29 +0200

    Scenario when null object do not throw Null Pointer Exception to access a field

---
+++ core-java/src/main/java/com/interview/nullobj/ValidAccessUsingNullObjectMain.java
@@ -0,0 +1,11 @@
+package com.interview.nullobj;
+
+public class ValidAccessUsingNullObjectMain {
+	private static final int ID = 1;
+	public static void main(String[] args) {
+		
+		ValidAccessUsingNullObjectMain nullObject = new ValidAccessUsingNullObjectMain();
+		System.out.println( "ID = " + nullObject.ID);
+	}
+
+}
